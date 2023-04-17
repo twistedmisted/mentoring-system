e0b60ca20790515e8eee25e1db4af6d3ee38fa6e
Andrii Mishchenko <mishchenko.andrii.02@gmail.com>
2023-04-17 17:33:26 +0300

    First commit

---
+++ src/main/java/ua/kpi/mishchenko/mentoringsystem/MentoringSystemApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.mishchenko.mentoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentoringSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kpi/mishchenko/mentoringsystem/MentoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.mishchenko.mentoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
