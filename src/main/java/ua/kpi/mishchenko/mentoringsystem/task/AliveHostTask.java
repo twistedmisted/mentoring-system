1595153e557cb8c1000201baf1f8a3c1f741f947
Andrii Mishchenko <mishchenko.andrii.02@gmail.com>
2023-05-03 20:50:45 +0300

    - Updated task

--- src/main/java/ua/kpi/mishchenko/mentoringsystem/task/AliveHostTask.java
+++ src/main/java/ua/kpi/mishchenko/mentoringsystem/task/AliveHostTask.java
@@ -1,19 +1,17 @@
 package ua.kpi.mishchenko.mentoringsystem.task;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-@RequiredArgsConstructor
 public class AliveHostTask {
 
-    private static final String GOOGLE_URL = "https://www.google.com/";
-    private static final RestTemplate REST_TEMPLATE = new RestTemplate();
+    private static final String URL = "https://attenbot.onrender.com/api/users";
 
-    @Scheduled(cron = "0 0/10 * * * *", zone = "Europe/Kiev")
+    @Scheduled(fixedDelay = 12 * 60 * 1000)
     public void run() {
-        REST_TEMPLATE.getForEntity(GOOGLE_URL, String.class);
+        RestTemplate restTemplate = new RestTemplate();
+        Integer users = restTemplate.getForObject(URL, Integer.class);
     }
 }
